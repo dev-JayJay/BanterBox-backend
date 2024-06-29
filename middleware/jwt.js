237dfc3a0b60ced60ef765cce75f9ec7f63a83f6
dev-jay-jay <johngratia2@gmail.com>
2024-06-29 14:45:13 +0100

    login route working with jwt

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -1,13 +1,24 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (token == null) return res.sendStatus(401);
+    // Get token from header
+    const token = req.header('Authorization')?.split(' ')[1];
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
+    // Check if token exists
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied, token missing' });
+    }
+
+    try {
+        // Verify token
+        const decoded = jwt.verify(token, process.env.JWT_PHASE); // Replace with your JWT secret key
+        req.user = decoded;
         next();
-    });
-}
+    } catch (error) {
+        console.error('Token verification error:', error);
+        res.status(401).json({ message: 'Invalid token' });
+    }
+};
+
 module.exports = authenticateToken;
