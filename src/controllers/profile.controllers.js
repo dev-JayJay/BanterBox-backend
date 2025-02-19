d3925cd4a9e3ed5956316b66c950d8eba48896b6
dev-jayjay <johngratia2@gmail.com>
2025-02-19 16:50:04 +0100

    user can edit their profile data

--- src/controllers/profile.controllers.js
+++ src/controllers/profile.controllers.js
@@ -4,12 +4,33 @@ import { responseMessage } from "../utils/response.util.js";
 
 export const getProfileControllers = async (request , response) => {
     const userId = request.user.user_id;
-    console.log(`this is the user id from the verifyToken function ${userId}`)
     try {
         const user = await User.findById(userId);
 
         responseMessage(response, "User Details", user, 200);
     } catch (error) {
         responseMessage(response, "Error getting profile details", null, 500);
+    }
+}
+
+export const updateUserProfileController = async (request , response) => {
+    const { user_id } = request.user;
+    const userDataToUpdate = request.body;
+
+    if (Object.keys(userDataToUpdate).length === 0) {
+        responseMessage(response, "Insert at least one data to update", null, 400);
+    }
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate( user_id, { $set: userDataToUpdate }, { new: true, runValidators: true } );
+        
+        if (!updatedUser) {
+            responseMessage(response, "cannot update user profile details", null, 500);
+        }
+
+        responseMessage(response, "user profile details updates successfully", updatedUser, 200);
+
+    } catch (error) {
+        responseMessage(response, "Error updating profile details", error, 500);
     }
 }
